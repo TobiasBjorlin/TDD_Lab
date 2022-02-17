b036b9f9689ebd0c927a05b0607b2484df88699c
tobbe <tobiasbjorlin@iths.se>
2022-02-17 11:48:08 +0100

    RED

--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -5,15 +5,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class EmployeeSystemTest {
 
     EmployeeSystem testObject;
-    Employee tobias = new Employee();
+    Employee tobias;
 
     @Test
     public void checkIfEmployeeAddedToArrayListTest(){
         testObject = new EmployeeSystem();
+        tobias = new Employee();
         testObject.addEmployeeToArrayList(tobias);
         boolean actual = testObject.employeeList.contains(tobias);
         boolean expected = true;
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkIfEmployeeHasName(){
+        tobias = new Employee();
+        boolean actual = tobias.name.isEmpty();
     }
 
 
