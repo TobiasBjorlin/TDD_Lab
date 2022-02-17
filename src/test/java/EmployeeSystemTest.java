364fb0c61d3cb872062cc497295a9b0a89f20854
tobbe <tobiasbjorlin@iths.se>
2022-02-17 11:36:34 +0100

    REFAC

--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -1,10 +1,8 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class EmployeeTest {
+public class EmployeeSystemTest {
 
     EmployeeSystem testObject;
     Employee tobias = new Employee();
