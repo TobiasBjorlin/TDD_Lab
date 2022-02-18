f039c64f226f87b9d777757bbbfdc3befc1b32f4
tobbe <tobiasbjorlin@iths.se>
2022-02-18 11:39:12 +0100

    RED

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -2,14 +2,15 @@
 public class Employee {
     String name;
     int age;
+    float salary;
     static int uID = 0;
     {
         uID += 1;
     }
-    public Employee(String eName, int eAge){
+    public Employee(String eName, int eAge, float eSalary){
         name = eName;
         age = eAge;
-
+        salary = eSalary;
     }
 
 }
--- src/test/java/EmployeeSystemTest.java
+++ src/test/java/EmployeeSystemTest.java
@@ -10,7 +10,7 @@ public class EmployeeSystemTest {
     @Test
     public void checkIfEmployeeAddedToArrayListTest(){
         testObject = new EmployeeSystem();
-        testEmployee = new Employee("Tobias", 26);
+        testEmployee = new Employee("Tobias", 26, 1000);
         testObject.addEmployeeToArrayList(testEmployee);
         boolean actual = testObject.employeeList.contains(testEmployee);
         boolean expected = true;
@@ -19,14 +19,14 @@ public class EmployeeSystemTest {
 
     @Test
     public void checkIfEmployeeHasName(){
-        testEmployee = new Employee("Tobias", 26);
+        testEmployee = new Employee("Tobias", 26, 10000);
         boolean actual = testEmployee.name.isEmpty();
         assertFalse(actual);
     }
 
     @Test
     public void checkIfEmployeeHasAge(){
-        testEmployee = new Employee("Tobias",26);
+        testEmployee = new Employee("Tobias",26, 1000);
         boolean actual;
         if(testEmployee.age > 0){
             actual = true;
@@ -39,18 +39,31 @@ public class EmployeeSystemTest {
 
     @Test
     public void checkIfEmployeeHasID(){
-        testEmployee = new Employee("Göran",53);
+        testEmployee = new Employee("Göran",53, 1000);
         System.out.println(testEmployee.uID);
-        testEmployee = new Employee("Göran",53);
-        testEmployee = new Employee("Göran",53);
-        testEmployee = new Employee("Göran",53);
+        testEmployee = new Employee("Göran",53, 1000);
+        testEmployee = new Employee("Göran",53, 1000);
+        testEmployee = new Employee("Göran",53, 1000);
         System.out.println(testEmployee.uID);
         boolean actual = testEmployee.uID >= 0;
         assertTrue(actual);
     }
 
 
+    @Test
+    public void checkIfEmployeeHasSalary(){
+        testEmployee = new Employee("Viktor", 26, 1000);
+        boolean actual = false;
+        if (testEmployee.salary > 0){
+            actual = true;
+        }
+        assertTrue(actual);
+    }
 
+    @Test
+    public void testGetAndSetForName(){
+        testEmployee.getName();
+    }
 
 }
 
